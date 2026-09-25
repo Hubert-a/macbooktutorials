403488e2c42f26820d4a81127548e21c4bce219f
Hubert-a <hubertamarfio@gmail.com>
2026-09-25 17:50:25 +0000

    Add Hero component and integrate it into the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import NavBar from "@/components/navbar";
+import Hero from "@/components/hero";
 export default function Home() {
   return (
     <main>
       <NavBar />
+      <Hero/>
     </main>
   );
 }
